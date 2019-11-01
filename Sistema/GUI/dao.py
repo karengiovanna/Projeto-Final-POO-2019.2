9528d260a30a998f8b6e8bf1855a80168372e764
Karen <giovannagiovanna13@gmail.com>
2019-11-01 11:26:03 -0400

    Inserção do banco de dados de veiculos

--- Sistema/GUI/dao.py
+++ Sistema/GUI/dao.py
@@ -130,3 +130,75 @@ class ClientDAO:
         "fechar o banco"
         print('Fechando o Banco...')
         self.bd.disconnect()
+
+class VeiculoDAO:
+    def __init__(self):
+        "Quando a aplicação for executada pela primeira vez, cria-se o banco de dados"
+        self.bd = BdAccess()
+        self.bd.connect()
+        self.bd.persist()
+
+    def view(self):
+        "recupera todos os dados do banco."
+        rows = None
+        try:
+            self.bd.execute("SELECT * FROM veiculo")    
+            rows = self.bd.fetchall()
+        except sql.Error as error:
+            print("Falha ao tentar selecionar os registros")
+            print("Classe da exceção: ", error.__class__)
+            print("Exceção é ", error.args)
+            raise sql.Error()
+        return rows
+
+    def insert(self, veiculo):
+        "insere novos registros no banco"
+        try:
+            self.bd.execute("INSERT INTO veiculo VALUES(NULL, ?,?,?,?,?,?,?,?,?,?,?,?,?)", (veiculo.marca, veiculo.modelo, veiculo.ano, veiculo.cor,veiculo.tanque, veiculo.combustivel, veiculo.consumo_cidade, veiculo.consumo_estrada, veiculo.tempo_0_100, veiculo.chassi, veiculo.placa, veiculo.tamanho_pneu,veiculo.som, veiculo.valor_diaria))
+            self.bd.persist()
+        except sqlite3.Error as error:
+            print("Falha ao tentar inserir os registros")
+            print("Classe da exceção: ", error.__class__)
+            print("Exceção é ", error.args)
+
+    def search(self, veiculo):
+        '''
+        A função de busca utiliza o operador OR e todos os campos 
+        que não forem preenchidos pelo usuário na hora da busca serão 
+        considerados como strings vazias
+        '''
+        rows = None
+        try:
+            self.bd.execute("SELECT * FROM veiculo WHERE marca=? or modelo=? or ano=? or cor=? or tanque =? or combustivel =? or consumo_cidade =? or consumo_estrada =? or tempo_0_100 =? or chassi =? or placa =? or tamanho_pneu =? or som =? or valor_diaria =?", (veiculo.marca, veiculo.modelo, veiculo.ano, veiculo.cor,veiculo.tanque, veiculo.combustivel, veiculo.consumo_cidade, veiculo.consumo_estrada, veiculo.tempo_0_100, veiculo.chassi, veiculo.placa, veiculo.tamanho_pneu,veiculo.som, veiculo.valor_diaria))
+            rows = self.bd.fetchall()
+        except sqlite3.Error as error:
+            print("Falha ao tentar buscar os registros")
+            print("Classe da exceção: ", error.__class__)
+            print("Exceção é ", error.args)
+            raise Exception
+        return rows
+
+    def update(self, id, veiculo):
+        "atualiza registros no banco"
+        try:
+            self.bd.execute("UPDATE veiculo SET marca =?, modelo=?, ano=?, cor=? WHERE id = ?",(veiculo.marca, veiculo.modelo, veiculo.ano, veiculo.cor,veiculo.tanque, veiculo.combustivel, veiculo.consumo_cidade, veiculo.consumo_estrada, veiculo.tempo_0_100, veiculo.chassi, veiculo.placa, veiculo.tamanho_pneu,veiculo.som, veiculo.valor_diaria, id))
+            self.bd.persist()
+        except sqlite3.Error as error:
+            print("Falha ao tentar atualizar os registros")
+            print("Classe da exceção: ", error.__class__)
+            print("Exceção é ", error.args)
+    
+    def delete(self, id):
+        "remove registros no banco"
+        try:
+            self.bd.execute("DELETE FROM veiculo WHERE id = ?", (id,))
+            self.bd.persist()
+        except sqlite3.Error as error:
+            print("Falha ao tentar remover o registro")
+            print("Classe da exceção: ", error.__class__)
+            print("Exceção é ", error.args)
+
+    def close(self):
+        "fechar o banco"
+        print('Fechando o Banco...')
+        self.bd.disconnect()
